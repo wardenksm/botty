4894f751314b10f217ab6641fb88be04708c7733
Roozhou <roozhou@gmail.com>
2022-07-08 23:58:49 -0700

    Automap pather for Nihlathak level 2

--- src/run/nihlathak.py
+++ src/run/nihlathak.py
@@ -1,15 +1,28 @@
 from char.i_char import IChar
+from config import Config
 from logger import Logger
 from pather import Location, Pather
 from item.pickit import PickIt
 import template_finder
 from town.town_manager import TownManager
-from utils.misc import wait
+from utils.custom_mouse import mouse
+from ui_manager import center_mouse
+from utils.misc import cut_roi, wait
 from dataclasses import dataclass
-from screen import convert_abs_to_monitor
-import random
+from screen import convert_abs_to_monitor, convert_abs_to_screen, convert_screen_to_abs, convert_screen_to_monitor, grab
+import random, keyboard, time, cv2, math
+import numpy as np
 
 from ui import loading, waypoint
+
+pillar_offsets = [
+    (-22, 16), #bottm
+    (97, 15), #right
+    (98, -69), #top
+    (-23, -69), #left
+]
+
+nih_loc_name = ["bottom", "right", "top", "left"]
 
 class Nihlathak:
 
@@ -28,6 +41,8 @@ class Nihlathak:
         self._char = char
         self._pickit = pickit
         self._runs = runs
+        self._roi = Config().ui_roi["cut_skill_bar"]
+        self._head_pos = convert_abs_to_monitor((0, -80))
 
     def approach(self, start_loc: Location) -> bool | Location:
         Logger.info("Run Nihlathak")
@@ -38,6 +53,44 @@ class Nihlathak:
         if waypoint.use_wp("Halls of Pain", curr_active_act = self._town_manager.get_act_idx_from_location(start_loc)): # use Halls of Pain Waypoint (5th in A5)
             return Location.A5_NIHLATHAK_START
         return False
+
+    def get_pillar_on_automap(self, automap_on=False):
+        ref = grab()
+        keyboard.send("tab")
+        time.sleep(0.04)
+        original = grab(force_new=True)
+        if automap_on:
+            ref, original = original, ref
+        img = original.copy()
+        if (match := template_finder.search(["NI2_STAIRS_AUTOMAP"], img, roi=self._roi, threshold=0.80, use_grayscale=False)).valid:
+            center = match.center
+        else:
+            # try small template
+            if not (match := template_finder.search(["NI2_STAIRS_SMALL_AUTOMAP"], img, roi=[540,260,200,200], threshold=0.92, use_grayscale=False, best_match=True)).valid:
+                cv2.imwrite(f"./debug_screenshots/nihlathak_failed_{time.strftime('%m%d_%H%M%S')}.png", cut_roi(img, self._roi))
+                return -1
+            center = (match.center[0] + 2, match.center[1] + 4)
+
+        diff_blue = np.int16(img[:,:,0]) - np.int16(ref[:,:,0])
+        min_sum = 1000000
+        empty_pillar = -1
+        for i, (x,y) in enumerate(pillar_offsets):
+            x += center[0]
+            y += center[1]
+            diff = np.clip(diff_blue[y-4:y+4,x-2:x+2].ravel(), 0, 255)
+            sum = np.sum(diff)
+            if sum < min_sum:
+                min_sum = sum
+                empty_pillar = i
+        '''
+        for i, (x,y) in enumerate(pillar_offsets):
+            x += center[0]
+            y += center[1]
+            cv2.rectangle(img, (x-3, y-5), (x+3, y+5), (0,0,255) if empty_pillar != i else (0,255,0), 1)
+        img = cv2.addWeighted(img, 0.5, original, 0.5, 0)
+        cv2.imwrite(f"./debug_screenshots/nihlathak_pillar_{time.strftime('%m%d_%H%M%S')}.png", cut_roi(img, self._roi))
+        '''
+        return empty_pillar
 
     def battle(self, do_pre_buff: bool) -> bool | tuple[Location, bool]:
         # TODO: We might need a second template for each option as merc might run into the template and we dont find it then
@@ -67,41 +120,48 @@ class Nihlathak:
         #wait(1.0) # wait to make sure the red writing is gone once we check for the eye
         @dataclass
         class EyeCheckData:
-            template_name: list[str]
             destination_static_path_key: str
+            dest_static_path: list[tuple[int, int]]
             circle_static_path_key: str
-            save_dist_nodes: list[int]
+            safe_dist_nodes: list[int]
             end_nodes: list[int]
 
         check_arr = [
-            EyeCheckData(["NI2_A_1_SAFE_DIST", "NI2_A_2_SAFE_DIST", "NI2_A_1_NOATTACK", "NI2_A_2_NOATTACK"], "ni2_a_safe_dist", "ni2_circle_a", [500], [501]),
-            EyeCheckData(["NI2_B_1_SAFE_DIST", "NI2_B_1_NOATTACK", "NI2_B_2_NOATTACK"], "ni2_b_safe_dist", "ni2_circle_b", [505], [506]),
-            EyeCheckData(["NI2_C_1_SAFE_DIST", "NI2_C_1_NOATTACK"], "ni2_c_safe_dist", "ni2_circle_c", [510], [511]),
-            EyeCheckData(["NI2_D_1_SAFE_DIST", "NI2_D_1_NOATTACK"], "ni2_d_safe_dist", "ni2_circle_d", [515], [516, 517]),
+            EyeCheckData("ni2_a_safe_dist", [(84, 710)] * 3, "ni2_circle_a", [1500], [501]),
+            EyeCheckData("ni2_b_safe_dist", [(1267, 595)] * 4, "ni2_circle_b", [1505], [506]),
+            EyeCheckData("ni2_c_safe_dist", [(1267, 13)] * 4, "ni2_circle_c", [1510], [511]),
+            EyeCheckData("ni2_d_safe_dist", [(65, 10)] * 3, "ni2_circle_d", [1515], [516, 517]),
         ]
 
-        end_nodes = None
-        for data in check_arr:
-            # Move to spot where eye would be visible
-            self._pather.traverse_nodes_fixed(data.circle_static_path_key, self._char)
-            # Search for eye
-            template_match = template_finder.search_and_wait(data.template_name, threshold=0.7, best_match=True, timeout=3)
-            # If it is found, move down that hallway
-            if template_match.valid and template_match.name.endswith("_SAFE_DIST"):
-                self._pather.traverse_nodes_fixed(data.destination_static_path_key, self._char)
-                self._pather.traverse_nodes(data.save_dist_nodes, self._char, timeout=2, do_pre_move=False)
-                end_nodes = data.end_nodes
-                break
-
-        # circle back and just assume path a if we failed to find the "eye"
-        if end_nodes is None:
-            self._pather.traverse_nodes_fixed("ni2_circle_back_to_a", self._char)
-            self._pather.traverse_nodes_fixed(check_arr[0].destination_static_path_key, self._char)
-            self._pather.traverse_nodes(check_arr[0].save_dist_nodes, self._char, timeout=2, do_pre_move=False)
-            end_nodes = check_arr[0].end_nodes
+        self._pather.traverse_nodes_fixed([(1270,10), (270,148), (120,670)], self._char)
+        if (nih_loc := self.get_pillar_on_automap(False)) < 0:
+            Logger.error("Could not find pillars on automap!")
+            keyboard.send("tab")
+            return (Location.A5_NIHLATHAK_END, False)
+        Logger.info(f"Found Nihlathak at {nih_loc_name[nih_loc]}")
+        self._pather.traverse_nodes_fixed(check_arr[nih_loc].dest_static_path[:1], self._char)
+        #self._pather.traverse_nodes_automap([1520+nih_loc], self._char, threshold=0.90, do_pre_move=False, toggle_map=False)
+        # Double check empty pillar
+        nih_loc_1 = self.get_pillar_on_automap(True)
+        path_to_safe_dist = check_arr[nih_loc].dest_static_path
+        if nih_loc_1 >= 0 and nih_loc_1 != nih_loc:
+            Logger.info(f"Nihlathak at {nih_loc_name[nih_loc_1]}? Check again...")
+            # move one more step try again
+            self._pather.traverse_nodes_fixed(check_arr[nih_loc].dest_static_path[0:1], self._char)
+            nih_loc_2 = self.get_pillar_on_automap(False)
+            if nih_loc_1 == nih_loc_2:
+                nih_loc = nih_loc_1
+                Logger.warning(f"Correct Nihlathak position to {nih_loc_name[nih_loc]}")
+                self._pather.traverse_nodes_automap([1520+nih_loc], self._char, threshold=0.90, do_pre_move=False, toggle_map=False)
+                path_to_safe_dist = check_arr[nih_loc].dest_static_path
+            else:
+                path_to_safe_dist = check_arr[nih_loc].dest_static_path[1:]
+            keyboard.send("tab")
+        self._pather.traverse_nodes_fixed(path_to_safe_dist, self._char)
+        self._pather.traverse_nodes_automap(check_arr[nih_loc].safe_dist_nodes, self._char, threshold=0.90, do_pre_move=False, force_tp = True)
 
         # Attack & Pick items
-        if not self._char.kill_nihlathak(end_nodes):
+        if not self._char.kill_nihlathak(check_arr[nih_loc].end_nodes):
             return False
         wait(0.2, 0.3)
         picked_up_items = self._pickit.pick_up_items(self._char)
